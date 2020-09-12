ffbfaf1cd349e37c7f69b990c5d98417cb10dbec
Majd Ajroudi <majd.ajroudi@gmail.com>
2020-09-12 16:59:07 +0300

    first modifications

--- src/product/index.js
+++ src/product/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
 const Product = () => {
+    const [isLiked, setIsLiked] = React.useState("false")
+    const textColor = isLiked? "blue": "black";
+    console.log(isLiked)
+    const handleLikeClick = () => {
+        setIsLiked("true")
+    }
     return (
         <>
-            <h2>a product</h2>
+            <h2>Name of the prodcut</h2>
+            <img src= "#"/>
+            <p>Description:</p>
+            <p>Instock products</p>
+            <p>number of likes</p>
+            <button onClick ={handleLikeClick} style={{color: {textColor}}}>Like!</button>
+            <button>Add to Cart</button>
         </>
     )
 }
